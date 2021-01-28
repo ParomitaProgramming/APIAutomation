359ac0d943ea6aea285a474c9c47fac5e6f469ff
Sayan Dasgupta <57595408+SayanDasguptaAutomation@users.noreply.github.com>
2021-01-28 15:50:33 +0530

    Update

--- src/main/java/com/qa/client/GetClient.java
+++ src/main/java/com/qa/client/GetClient.java
@@ -1,6 +1,8 @@
 package com.qa.client;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Set;
 
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -8,6 +10,8 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class GetClient {
 
@@ -23,6 +27,63 @@ public class GetClient {
 		String response = EntityUtils.toString(httpResponse.getEntity(),"UTF-8");
 		
 		return response;
+	}
+	
+	public String NestedJSONResponse(String resp, String jPath)
+	{
+		JSONObject jObj = new JSONObject(resp);
+		String response = jObj.get(jPath).toString();
+		
+		return response;
+	}
+	
+	public HashMap<String, String> FetchValueByField(String resp, Set<Object> keyResp, String fieldName)
+	{
+		 HashMap<String, String> valueList = new HashMap<String, String>();
+		 
+		 if(resp.contains("[") || resp.contains("]"))
+		 {
+			 HashMap<String, String> myList = new HashMap<String, String>();
+			 JSONArray jsonArray = new JSONArray(resp);
+			 for(int i=0;i<jsonArray.length();i++)
+			 {
+				 JSONObject jsonObj = jsonArray.getJSONObject(i);
+				 myList.put(Integer.toString(i), jsonObj.toString());
+				 
+			 }
+			 
+			 String findKey = null;
+			 
+			 for(int i=0;i<myList.size();i++)
+			 {
+				 for(Object j: keyResp)
+				 {
+					 JSONObject myObj = new JSONObject(myList.get(Integer.toString(i)));
+					 if(myObj.get(j.toString()).toString().equalsIgnoreCase(fieldName))
+					 {
+						 findKey = Integer.toString(i);
+					 }
+				 }
+			 }
+			 
+			 JSONObject respObj = new JSONObject(myList.get(findKey));
+			 for(Object j: keyResp)
+			 {
+				 valueList.put(j.toString(), respObj.get(j.toString()).toString());
+			 }
+			 
+		 }
+		 else
+		 {
+			 JSONObject respObj = new JSONObject(resp);
+			 for(Object j: keyResp)
+			 {
+				 valueList.put(j.toString(), respObj.get(j.toString()).toString());
+			 }
+		 }
+	
+		 return valueList;
+		 
 	}
 	
 }
--- src/test/java/com/qa/tests/APIGetTest.java
+++ src/test/java/com/qa/tests/APIGetTest.java
@@ -4,7 +4,10 @@ import org.testng.annotations.Test;
 import com.qa.base.TestBase;
 import com.qa.client.GetClient;
 
+import junit.framework.Assert;
+
 import java.io.IOException;
+import java.util.HashMap;
 
 import org.apache.http.client.ClientProtocolException;
 import org.testng.annotations.BeforeTest;
@@ -25,9 +28,19 @@ public class APIGetTest extends TestBase {
 		getClient = new GetClient();
 		String apiResponse = getClient.GetConnectionStatus(apiURL);
 		
+		apiResponse = getClient.NestedJSONResponse(apiResponse, "data");
 		
+		String ID = dataProp.getProperty("id");
+		String FirstName = dataProp.getProperty("first_name");
+		String LastName = dataProp.getProperty("last_name");
+		String Email = dataProp.getProperty("email");
+		
+		HashMap<String, String> valueList = getClient.FetchValueByField(apiResponse, dataProp.keySet(), ID);
+		
+		Assert.assertEquals(valueList.get("id"), ID);
+		Assert.assertEquals(valueList.get("first_name"), FirstName);
+		Assert.assertEquals(valueList.get("last_name"), LastName);
+		Assert.assertEquals(valueList.get("email"), Email);
 	}
-
-
 
 }
